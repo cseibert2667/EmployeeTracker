b7647882b3a9e53d96e1f759d02a77ef72527143
cseibert2667 <cseibert2667@gmail.com>
2020-09-07 21:21:32 -0700

    main menu prompts; added queries.js to store/export db queries

--- server.js
+++ server.js
@@ -14,10 +14,45 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected Successfully");
     mainMenu();
-    connection.end();
 });
 
 function mainMenu() {
-  inquirer.prompt()
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View All Employees", "View All Employees by Role", "View All Employees by Department", "Add Employee", "Add Role", "Add Department", "Update Employee Role", "Quit"],
+      name: "menuOption"
+    }
+  ]).then(function(response){
+    switch (response.menuOption) {
+      case "View All Employees":
+        viewEmps();
+        break;
+      case "View All Employees by Role":
+        viewRoles();
+        break;
+      case "View All Employees by Department":
+        viewDepts();
+        break;
+      case "Add Employee":
+        addEmp();
+        break;
+      case "Add Role":
+        addRole();
+        break;
+      case "Add Department":
+        addDept();
+        break;
+      case "Update Employee Role":
+        updateRole();
+        break;
+      case "Quit":
+        connection.end();
+    }
+  })
 }
 
+function viewEmps() {
+  connection.query("")
+}
